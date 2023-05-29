f3f8747347944d4425cbe5776e72563c82f6d998
walid hamdi <walidhamdidev@gmail.com>
2023-05-29 22:18:17 +0100

    Add register user

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-
 import NextLink from "next/link";
-
+import { useFormik } from "formik";
+// import * as Yup from "yup";
 import {
   Flex,
   Box,
@@ -14,10 +14,97 @@ import {
   Heading,
   Text,
   useColorModeValue,
+  useToast,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import GoogleButton from "../../components/GoogleButton";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+import axios from "axios";
+import { useState } from "react";
+
+interface RegisterFormValues {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 export default function Register() {
+  const router = useRouter();
+  const [isLoading, setLoading] = useState(false);
+  const toast = useToast();
+
+  const formik = useFormik<RegisterFormValues>({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validate: (values) => {
+      const errors: Partial<RegisterFormValues> = {};
+
+      if (!values.name) {
+        errors.email = "Name is required";
+      }
+      if (!values.email) {
+        errors.email = "Email is required";
+      }
+
+      if (!values.password) {
+        errors.password = "Password is required";
+      }
+      if (!values.confirmPassword) {
+        errors.password = "Confirm password is required";
+      }
+
+      if (values.password !== values.confirmPassword) {
+        errors.confirmPassword = "Passwords must match";
+      }
+
+      return errors;
+    },
+
+    onSubmit: async (values) => {
+      try {
+        setLoading(true);
+        const response = await axios.post(
+          "https://comminq-backend.onrender.com/api/user/register",
+          {
+            name: values.name,
+            email: values.email,
+            password: values.password,
+          }
+        );
+
+        const token = response.data.token;
+
+        Cookies.set("comminq_auth_token", token);
+        setLoading(false); // End loading state
+        router.replace("/");
+      } catch (error: any) {
+        let errorMessage = "An error occurred during register.";
+
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.error
+        ) {
+          errorMessage = error.response.data.error;
+        }
+        toast({
+          title: "Error",
+          description: errorMessage,
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+        setLoading(false); // End loading state
+      }
+    },
+  });
+
   return (
     <Flex
       minH={"100vh"}
@@ -37,38 +124,89 @@ export default function Register() {
           boxShadow={"lg"}
           p={8}
         >
-          <Stack spacing={4}>
-            <FormControl id="name">
-              <FormLabel>Name</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <FormControl id="confirmPassword">
-              {" "}
-              {/* Added confirm password */}
-              <FormLabel>Confirm Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Button
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
+          <form onSubmit={formik.handleSubmit}>
+            <Stack spacing={4}>
+              <FormControl
+                id="name"
+                isInvalid={!!formik.errors.name && formik.touched.name}
               >
-                Sign up
-              </Button>
-              <GoogleButton />
+                <FormLabel>Name</FormLabel>
+                <Input
+                  type="text"
+                  id="name"
+                  name="name"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.name}
+                />
+                <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
+              </FormControl>
+              <FormControl
+                id="email"
+                isInvalid={!!formik.errors.email && formik.touched.email}
+              >
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type="email"
+                  id="email"
+                  name="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              </FormControl>
+              <FormControl
+                id="password"
+                isInvalid={!!formik.errors.password && formik.touched.password}
+              >
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                />
+                <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+              </FormControl>
+              <FormControl
+                id="confirmPassword"
+                isInvalid={
+                  !!formik.errors.confirmPassword &&
+                  formik.touched.confirmPassword
+                }
+              >
+                <FormLabel>Confirm Password</FormLabel>
+                <Input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.confirmPassword}
+                />
+                <FormErrorMessage>
+                  {formik.errors.confirmPassword}
+                </FormErrorMessage>
+              </FormControl>
+              <Stack spacing={10}>
+                <Button
+                  type="submit"
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                  isLoading={formik.isSubmitting}
+                >
+                  Sign up
+                </Button>
+                <GoogleButton />
+              </Stack>
             </Stack>
-          </Stack>
+          </form>
         </Box>
 
         <Text fontSize={"lg"} color={"gray.600"}>
