2f914f7ea543b14af52a0d68071694514e0ed7b0
walid-hamdi <walidhamdidev@gmail.com>
2024-02-13 19:47:34 +0100

    change forget password

--- src/app/reset-password/_components/ResetPasswordBody/index.ts
+++ src/app/reset-password/_components/ResetPasswordBody/index.ts
@@ -1,35 +1,36 @@
 import React from "react";
 
 const ResetPasswordBody = () => {
-  return    <FormControl id="newPassword">
-            <Box py={4}>
-              <Input
-                placeholder="New Password"
-                _placeholder={{ color: "gray.500" }}
-                type="password"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-              />
-            </Box>
-          </FormControl>
+  return;
+  // return    <FormControl id="newPassword">
+  //           <Box py={4}>
+  //             <Input
+  //               placeholder="New Password"
+  //               _placeholder={{ color: "gray.500" }}
+  //               type="password"
+  //               value={newPassword}
+  //               onChange={(e) => setNewPassword(e.target.value)}
+  //             />
+  //           </Box>
+  //         </FormControl>
 
-          <Stack spacing={6}>
-            <Button
-              bg={"blue.400"}
-              color={"white"}
-              _hover={{
-                bg: "blue.500",
-              }}
-              onClick={handleResetPasswordClick}
-              isLoading={loading}
-              loadingText="Resetting"
-            >
-              Reset Password
-            </Button>
-            <Button variant="link" onClick={() => setStep("verification")}>
-              Go Back
-            </Button>
-          </Stack>
+  //         <Stack spacing={6}>
+  //           <Button
+  //             bg={"blue.400"}
+  //             color={"white"}
+  //             _hover={{
+  //               bg: "blue.500",
+  //             }}
+  //             onClick={handleResetPasswordClick}
+  //             isLoading={loading}
+  //             loadingText="Resetting"
+  //           >
+  //             Reset Password
+  //           </Button>
+  //           <Button variant="link" onClick={() => setStep("verification")}>
+  //             Go Back
+  //           </Button>
+  //         </Stack>
 };
 
 export default ResetPasswordBody;
